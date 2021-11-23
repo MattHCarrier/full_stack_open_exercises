b664f6efb111997b68a736a3701c787699f7a5f7
matthcarrier <matieunibre183@gmail.com>
2021-11-23 15:47:03 -0800

    working on solutions

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,18 +6,50 @@ const App = () => {
   ])
   const [ newName, setNewName] = useState('')
 
+    const addName = (event) => {
+      event.preventDefault()
+        const nameObject ={
+          name: newName
+        }
+
+        let checkDuplicate = persons.some(person =>{
+            return JSON.stringify(nameObject) === JSON.stringify(person)
+        })
+        if(!checkDuplicate) {
+            setPersons(persons.concat(nameObject))
+            setNewName('')
+        } else {
+            window.alert(`${nameObject.name} is already in the phonebook`)
+        }
+    }
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
   return(
       <div>
         <h2>Phonebook</h2>
-        <form>
+        <form onSubmit={addName}>
           <div>
-            name : <input/>
+            <input
+                value={newName}
+                onChange={handleNameChange}
+            />
+              <input
+                  value={newNumber}
+                  onChange={handleNumberChange}
+              />
           </div>
           <div>
             <button type={"submit"}>add</button>
           </div>
         </form>
         <h2>Numbers</h2>
+          <ul>
+              {persons.map(person => <li key={person.name}>{person.name}</li>)}
+
+          </ul>
       </div>
   )
 }
